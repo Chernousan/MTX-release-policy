3e998862a7431a3e9ee3e7a60d4ada2f8cbb8f57
Dmytro Chernousan <DChernousan@hstechnology.cz>
2025-09-02 09:14:55 +0200

    Added workflow feature

---
+++ settings.py
@@ -0,0 +1,12 @@
+"""
+Semantic version specification:
+<MAJOR>.<MINOR>.<PATCH>-<RELEASE>.<BUILD>
+
+MAJOR - major number. Change hen you make incompatible changes.
+MINOR - minor number. Change when you add new functionality.
+PATCH - patch or hotfix
+RELEASE - alpha, beta, rc or release
+BUILD - build number
+"""
+
+VERSION = "v.1.0.0"
